b2aaa85bb88780e05b60112fd964185d8403167e
voxlol <voxlol@outlook.com>
2015-09-04 11:52:46 -0500

    Setup express

--- geolocation.js
+++ geolocation.js
@@ -1,1 +1,13 @@
 // Google API Key - API_KEY
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// Middle-ware
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Server running on port 3000')
+});
